f7f041f52d64009e91a106e5ee5e56ec9722b518
郭亮 <guoliang@51bsi.com>
2018-07-11 14:23:03 +0800

    抽取分页功能到view中去

---
+++ 分页条控制器/PageSelect/PageSelect.h
@@ -0,0 +1,13 @@
+//
+//  PageSelect.h
+//  分页条控制器
+//
+//  Created by lang on 2018/7/11.
+//  Copyright © 2018年 cnovit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageSelect : UIView
+
+@end
---
+++ 分页条控制器/PageSelect/PageSelect.m
@@ -0,0 +1,21 @@
+//
+//  PageSelect.m
+//  分页条控制器
+//
+//  Created by lang on 2018/7/11.
+//  Copyright © 2018年 cnovit. All rights reserved.
+//
+
+#import "PageSelect.h"
+
+@implementation PageSelect
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
